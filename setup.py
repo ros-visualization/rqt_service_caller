28e02da4843b2b5e9e71c1b33bcd074126ed63d0
mosfet80 <10235105+mosfet80@users.noreply.github.com>
2025-07-05 10:25:19 +0200

    fix setuptools deprecations
    FIX:
    
    #UserWarning: Unknown distribution option: 'tests_require'
    
    and
    
    #SetuptoolsDeprecationWarning: License classifiers are deprecated. !!
    
    ********************************************************************************
    Please consider removing the following classifiers in favor of a SPDX license expression:
    
    License :: OSI Approved :: BSD License
    
    See https://packaging.python.org/en/latest/guides/writing-pyproject-toml/#license for details.
    ********************************************************************************

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ setup(
     keywords=['ROS'],
     classifiers=[
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
         'Topic :: Software Development',
     ],
@@ -31,7 +30,11 @@ setup(
         'rqt_service_caller provides a GUI plugin for calling arbitrary services.'
     ),
     license='BSD',
-    tests_require=['pytest'],
+    extras_require={
+        'test': [
+            'pytest'
+        ]
+    },
     entry_points={
         'console_scripts': [
             'rqt_service_caller = ' + package_name + '.main:main',
