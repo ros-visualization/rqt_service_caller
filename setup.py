e9fcb798c454d3f906e0656301e0e45f249af73e
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2026-06-17 12:20:03 +0200

    1.5.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ package_name = 'rqt_service_caller'
 
 setup(
     name=package_name,
-    version='1.5.2',
+    version='1.5.3',
     packages=[package_name],
     package_dir={'': 'src'},
     data_files=[
